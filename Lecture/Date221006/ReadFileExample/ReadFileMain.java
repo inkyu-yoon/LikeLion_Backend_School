ecb49ad321ccbd0071d758580b9f1300b4ad4305
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-06 15:15:09 +0900

    콘솔 화면 포함

--- Lecture/Date221006/ReadFileExample/ReadFileMain.java
+++ Lecture/Date221006/ReadFileExample/ReadFileMain.java
@@ -11,14 +11,21 @@ public class ReadFileMain {
         for (File file : files) {
             System.out.println("file = " + file);
         }
-
         ReadFile readFile = new ReadFile("height.txt"); // 읽어올 텍스트 파일이 프로젝트 최 상단 폴더에 존재해야함
         System.out.println(readFile.readOneWord());
         System.out.println(readFile.readNWord(5));
         System.out.println(readFile.readNWord(10));
-
-
     }
-
-
 }
+/*
+file = .\.git
+file = .\.gitignore
+file = .\.idea
+file = .\Assignment
+file = .\height.txt
+file = .\Lecture
+file = .\out
+i
+id	hi
+id	hid	hei
+ */
