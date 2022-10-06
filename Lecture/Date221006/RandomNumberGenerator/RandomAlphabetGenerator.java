5deb3f7f26f6166316211a15032004d4c5451254
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-06 10:34:58 +0900

    중복 제거 코드 정리

--- Lecture/Date221006/RandomNumberGenerator/RandomAlphabetGenerator.java
+++ Lecture/Date221006/RandomNumberGenerator/RandomAlphabetGenerator.java
@@ -3,6 +3,6 @@ package Lecture.Date221006.RandomNumberGenerator;
 public class RandomAlphabetGenerator implements NumberGenerator {
     @Override
     public int generate(int num) {
-        return (int)(Math.random() * num)+65;
+        return (int)(Math.random() * num)+65; //알파벳 대문자 아스키 코드는 65~90 이므로
     }
 }
--- Lecture/Date221006/RandomNumberGenerator/RndNumbersWithoutDuplicated.java
+++ Lecture/Date221006/RandomNumberGenerator/RndNumbersWithoutDuplicated.java
@@ -5,17 +5,21 @@ import java.util.Set;
 
 public class RndNumbersWithoutDuplicated {
     public static void main(String[] args) {
+        //1. 랜덤 숫자 생성 및 중복 제거
         NumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-        NumberGenerator randomAlphabetGenerator = new RandomAlphabetGenerator();
         Set<Integer> randomNumberSet = new HashSet<>();
         for (int i = 0; i < 50; i++) {
-            int r = randomNumberGenerator.generate(10);
+            int r = randomNumberGenerator.generate(30);
+            System.out.println("r = " + r);
             randomNumberSet.add(r);
         }
+        System.out.println("============결과============");
         for (Integer integer : randomNumberSet) {
-            System.out.println(integer);
+            System.out.print(integer + " ");
         }
 
+        //2. 랜덤 알파벳 생성 및 중복 제거
+        NumberGenerator randomAlphabetGenerator = new RandomAlphabetGenerator();
         Set<Character> randomAlphabetSet = new HashSet<>();
         for (int i = 0; i < 50; i++) {
             int r = randomAlphabetGenerator.generate(26);
