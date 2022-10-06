56718ba35230d4f4e0b80a57d8f502ca54ed7f46
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-06 14:41:34 +0900

    글자 n개 읽어오기

--- Lecture/Date221006/ReadFileExample/ReadFile.java
+++ Lecture/Date221006/ReadFileExample/ReadFile.java
@@ -10,9 +10,18 @@ public class ReadFile {
     ReadFile(String fileName) {
         this.fileName=fileName;
     }
-     char readOneByte() throws IOException {
+    char readOneWord() throws IOException {
         //지정한 파일의 맨 앞 한 byte를 읽어오는 메서드를 만들기
         BufferedReader br = new BufferedReader(new FileReader(this.fileName));
         return (char)br.read();
+    }
+    String readNWord(int N) throws IOException {
+        //지정한 파일의 맨 앞 한 byte를 읽어오는 메서드를 만들기
+        BufferedReader br = new BufferedReader(new FileReader(this.fileName));
+        String output ="";
+        for (int i = 0; i < N; i++) {
+            output+=(char)(br.read());
+        }
+        return output;
     }
 }
--- Lecture/Date221006/ReadFileExample/ReadFileMain.java
+++ Lecture/Date221006/ReadFileExample/ReadFileMain.java
@@ -7,9 +7,10 @@ import java.io.IOException;
 public class ReadFileMain {
     public static void main(String[] args) throws IOException {
         //한 바이트 읽기
-
-        ReadFile readFile = new ReadFile("C:\\Users\\ikyoo\\Desktop\\멋쟁이사자처럼 백엔드 스쿨\\LikeLion_Backend_School\\Lecture\\Date221006\\ReadFileExample\\test.txt");
-        System.out.println(readFile.readOneByte());
+        String filePath = "C:\\Users\\ikyoo\\Desktop\\멋쟁이사자처럼 백엔드 스쿨\\LikeLion_Backend_School\\Lecture\\Date221006\\ReadFileExample\\test.txt";
+        ReadFile readFile = new ReadFile(filePath);
+        System.out.println(readFile.readOneWord());
+        System.out.println(readFile.readNWord(5));
     }
 
 
