9a0271dba7593f1116753ed70d139970559729aa
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-07 14:05:53 +0900

    *강사님 방법이랑 약간 다름* 주석 정리

--- Lecture/Date221007/ReadBigDataProject/PopulationStatistics.java
+++ Lecture/Date221007/ReadBigDataProject/PopulationStatistics.java
@@ -10,42 +10,50 @@ public class PopulationStatistics {
     }
 
     public static void main(String[] args) throws IOException {
-        ReadAction readAction1 = new ReadAction("./BigDataRepository.csv");
-        ReadAction readAction2 = new ReadAction("./BigDataRepository.csv");
+        ReadAction readActionAboutFrom = new ReadAction("./BigDataRepository.csv");
+        //↑ 전입 데이터 전용 객체
+        ReadAction readActionAboutTo = new ReadAction("./BigDataRepository.csv");
+        //↑ 전출 데이터 전용 객체
 
         int[] cityCode = {11, 21, 22, 23, 24, 25, 26, 29, 31, 32, 33, 34, 35, 36, 37, 38, 39};
         String[] cityName = {"서울", "부산", "대구", "인천", "광주", "대전", "울산", "세종", "경기도", "강원도", "충북", "충남", "전북", "전남", "경북", "경남", "제주"};
 
         //빅데이터의 알고싶은 코드의 *인덱스*와, 각 *코드*가 *의미하는 내용*에 대한 배열을 입력하면 출력할 수 있음.
         //자유롭게 정보 출력 가능 -> 조금 더더 객체지향적으로도 만들 수는 있을 듯....?
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
+
+        StringBuilder sbAboutFrom = new StringBuilder();
+        //↑ 전입 데이터 전용
+        StringBuilder sbAboutTo = new StringBuilder();
+        //↑ 전출 데이터 전용
 
 
-        sb1=readAction1.getBigDataWithIndex(cityCode, cityName, 0);
-        // ↑ 알고싶은 인덱스는 0번째 인덱스, 해당 인덱스의 데이터에 대한 정보 배열 입력
+        sbAboutFrom=readActionAboutFrom.getBigDataWithIndex(cityCode, cityName, 0);
+        // ↑ 알고싶은 인덱스는 0번째 인덱스, 해당 인덱스의 데이터에 대한 정보를 StringBuilder 에 저장한 후 반환
+        sbAboutTo=readActionAboutTo.getBigDataWithIndex(cityCode, cityName, 6);
+        // ↑ 알고싶은 인덱스는 6번째 인덱스, 해당 인덱스의 데이터에 대한 정보를 StringBuilder 에 저장한 후 반환
 
-        sb2=readAction2.getBigDataWithIndex(cityCode, cityName, 6);
 
+        //텍스트 파일에 저장하는 과정
         String writeFileName = "from_to.txt";
         createAFile(writeFileName); //"텍스트 파일 생성"
         BufferedWriter bw = new BufferedWriter(new FileWriter(new File(writeFileName)));
-        //파일 경로 지정, 이제 bw.write 로 써주기만 하면 됨
+        //↑ 파일 경로 지정, 이제 bw.write 로 써주기만 하면 됨
 
-        bw.write(sb1.append(sb2).toString());
+        bw.write(sbAboutFrom.append(sbAboutTo).toString());
         bw.close();
-        //텍스트 파일에 정보 저장 완료.
+        //↑ 텍스트 파일에 정보 저장 완료.
 
 
 
-        MapData map1 = new MapData(readAction1.getCountMap());
-        MapData map2 = new MapData(readAction2.getCountMap());
+        MapData mapAboutFrom = new MapData(readActionAboutFrom.getCountMap());
+        MapData mapAboutTo = new MapData(readActionAboutTo.getCountMap());
 //        map1.printInfo();
 //        map2.printInfo();
 
-        //만약 키 값이 11인 전입자 데이터를 구하고 싶다면 map1이 전입자 데이터이므로 키 값을 이용해서 찾는다.
-        System.out.println(map1.countMap.get(11));
-
+        System.out.println(mapAboutFrom.countMap.get(11));
+        //↑만약 키 값이 11인 전입자 데이터를 구하고 싶다면
+        System.out.println(mapAboutTo.countMap.get(26));
+        //↑만약 키 값이 26인 전출자 데이터를 구하고 싶다면
     }
 }
 /*
