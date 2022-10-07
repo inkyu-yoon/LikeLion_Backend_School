8b26134df1feb4a946517a5335e874f26614984e
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-07 23:09:18 +0900

    22-10-07 금요일 과제 코드업 1031 ~ 1040

---
+++ Assignment/CodeUp1031_1040/No_1033.java
@@ -0,0 +1,11 @@
+package Assignment.CodeUp1031_1040;
+
+import java.util.Scanner;
+
+public class No_1033 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.printf("%X", N);
+    }
+}
