35ab6ce4a5be9a6f2c73621dfe8311ff76bea730
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-07 10:32:39 +0900

    빅데이터에서 전입 코드 전출 코드 추출하기

--- Lecture/Date221007/ReadBigDataProject/ReadAction.java
+++ Lecture/Date221007/ReadBigDataProject/ReadAction.java
@@ -1,9 +1,13 @@
 package Lecture.Date221007.ReadBigDataProject;
 
+import Lecture.Date221006.ReadFileExample.Student;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReadAction {
     private String fileAddress;
@@ -35,6 +39,48 @@ public class ReadAction {
         }
     }
 
+    void getAllInfo() throws IOException{
+
+        String str;
+        while((str=br.readLine()) !=null){
+            String[] input = str.split(",");
+            int inCity = Integer.valueOf(input[0]);
+            int outCity = Integer.valueOf(input[6]);
+            DataInfo dataInfo = new DataInfo(inCity, outCity);
+            System.out.println(dataInfo);
+        }
+
+
+    }
+
+    void getDataMap() throws IOException {
+        Map<Integer, Integer> countInCityCode = new HashMap<>();
+        Map<Integer, Integer> countOutCityCode = new HashMap<>();
+        //행정구역 코드 서울 : 11 부산 : 21 대구 : 22 인천 :23 광주 : 24 대전 : 25 울산 :26
+        // 세종 : 29 경기도 : 31 강원도 :32 충북 : 33 충남 :34 전북 : 35 전남 : 36 경북 : 37 경남 : 38 제주 : 39
+        String str;
+        while ((str = br.readLine()) != null) {
+            String[] input = str.split(",");
+            int inCity = Integer.valueOf(input[0]);
+            int outCity = Integer.valueOf(input[6]);
+            DataInfo dataInfo = new DataInfo(inCity, outCity);
+
+            if(countInCityCode.containsValue(inCity)) {
+                countInCityCode.put(inCity, countInCityCode.get(inCity) + 1);
+            }else{
+                countInCityCode.put(inCity, 1);
+            }
+            if(countOutCityCode.containsValue(outCity)) {
+                countOutCityCode.put(inCity, countOutCityCode.get(outCity) + 1);
+            }else{
+                countOutCityCode.put(outCity, 1);
+            }
+//            System.out.println(dataInfo);
+        }
+        System.out.println(countOutCityCode);
+        System.out.println(countInCityCode);
+
+    }
 
 
 }
