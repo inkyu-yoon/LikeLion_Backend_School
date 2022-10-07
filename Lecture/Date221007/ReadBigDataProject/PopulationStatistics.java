e843216ef4dcb95462e788db21964d04adb563f2
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-07 11:12:07 +0900

    전입 전출 데이터 행정구역명으로 표시

--- Lecture/Date221007/ReadBigDataProject/PopulationStatistics.java
+++ Lecture/Date221007/ReadBigDataProject/PopulationStatistics.java
@@ -1,24 +1,17 @@
 package Lecture.Date221007.ReadBigDataProject;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class PopulationStatistics {
     public static void main(String[] args) throws IOException {
         ReadAction readAction = new ReadAction("./BigDataRepository.csv");
-//        readAction.readOneLine();
-//        readAction.readNLine(10);
-//        readAction.getAllInfo();
-        readAction.getDataMap();
+        readAction.getDataInAndOut();
     }
 }
 /*
 출력화면
 
-=========Show Data===========
-{32=0, 33=0, 34=0, 35=0, 36=49219, 37=0, 38=0, 39=0, 11=1227491, 21=0, 22=0, 23=0, 24=0, 25=0, 26=403502, 29=183625, 31=127846}
-{32=0, 33=0, 34=0, 35=0, 36=57000, 37=0, 38=0, 39=0, 11=1189998, 21=0, 22=0, 23=0, 24=0, 25=0, 26=388869, 29=179158, 31=118237}
+
 =======전입 데이터를 표시합니다.========
 서울 (으)로 1189998 명이 전입했습니다.
 부산 (으)로 0 명이 전입했습니다.
@@ -59,5 +52,6 @@ public class PopulationStatistics {
 제주 (으)로 0 명이 전출했습니다.
 
 Process finished with exit code 0
+
 
  */
