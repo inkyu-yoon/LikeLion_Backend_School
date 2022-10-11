c8af83ac5729728671c92889f85c56e74688f0f0
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-11 09:59:29 +0900

    자릿수 합 1자리 될 때까지 코드업 문제 1620번

---
+++ Lecture/Date221011/findDigitSum/CalculateDigitSum.java
@@ -0,0 +1,24 @@
+package Lecture.Date221011.findDigitSum;
+
+import java.util.Scanner;
+
+public class CalculateDigitSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        while(String.valueOf(findSum(N)).length()!=1) {
+            N=findSum(N);
+        }
+        System.out.println(findSum(N));
+        
+    }
+
+    static int findSum(int N) {
+        int result = 0;
+        while (N != 0) {
+            result += N % 10;
+            N /= 10;
+        }
+        return result;
+    }
+}
