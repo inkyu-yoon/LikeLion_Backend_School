a59d2202f911107317f0232da7042d877977776e
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-11 16:39:32 +0900

    22-10-11 화요일 과제 코드업 1041 ~ 1050

---
+++ Assignment/CodeUp1041_1050/No_1042.java
@@ -0,0 +1,12 @@
+package Assignment.CodeUp1041_1050;
+
+import java.util.Scanner;
+
+public class No_1042 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a / b);
+    }
+}
---
+++ Assignment/CodeUp1041_1050/No_1046.java
@@ -0,0 +1,11 @@
+package Assignment.CodeUp1041_1050;
+
+import java.util.Scanner;
+
+public class No_1046 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        System.out.println(a*2);
+    }
+}
