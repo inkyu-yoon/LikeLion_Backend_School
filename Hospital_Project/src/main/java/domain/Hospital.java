3b6ae03a74057b8cee83e7a22be679fb891f2d26
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-13 10:50:21 +0900

    서울시 병의원 정보 프로젝트 + 테스트 코드

---
+++ Hospital_Project/src/main/java/domain/Hospital.java
@@ -0,0 +1,56 @@
+package domain;
+
+public class Hospital {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public char getCategory() {
+        return category;
+    }
+
+    public int getEmergencyRoom() {
+        return emergencyRoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubdivision() {
+        return subdivision;
+    }
+
+    private String address;
+    private String district; //address에서 추출
+    private char category;
+    private int emergencyRoom;
+    private String name;
+    private String subdivision; //병원 이름 기준으로 채워보기
+
+    @Override
+    public String toString() {
+        return "(" + "\'"+ id +"\',"+ "\'"+ address +"\',"+ "\'"+ district +"\',"+ "\'"+ category +"\',"+
+                "\'"+ emergencyRoom +"\',"+ "\'"+ name +"\'," + "\'"+ subdivision +"\'"+ ")";
+    }
+
+    public Hospital(String id, String address, String district, char category, int emergencyRoom, String name, String subdivision) {
+        this.id = id;
+        this.address = address;
+        this.district = district;
+        this.category = category;
+        this.emergencyRoom = emergencyRoom;
+        this.name = name;
+        this.subdivision = subdivision;
+    }
+}
