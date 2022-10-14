6e6ac7db22395a2bf4a3f577ed665d39382ee182
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-14 09:29:50 +0900

    Insertion Sort 출력문 추가

--- LikeLionClass/October/Lecture/Date221014/insertion/InsertionSortMain.java
+++ LikeLionClass/October/Lecture/Date221014/insertion/InsertionSortMain.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class InsertionSortMain {
     public static void main(String[] args) {
-        int[] arr = {7, 2, 3, 9, 28, 11};
+        int[] arr = {8, 5, 6, 2, 4};
 
         arr = insertionSort(arr);
 
@@ -12,18 +12,53 @@ public class InsertionSortMain {
 
     }
 
+    /* 출력화면
+i = 1  ||  arr[0] vs arr[1]  ||  5 vs 8
+[5, 8, 6, 2, 4]
+
+i = 2  ||  arr[1] vs arr[2]  ||  6 vs 8
+[5, 6, 8, 2, 4]
+i = 2  ||  arr[0] vs arr[1]  ||  5 vs 6
+[5, 6, 8, 2, 4]
+
+i = 3  ||  arr[2] vs arr[3]  ||  2 vs 8
+[5, 6, 2, 8, 4]
+i = 3  ||  arr[1] vs arr[2]  ||  2 vs 6
+[5, 2, 6, 8, 4]
+i = 3  ||  arr[0] vs arr[1]  ||  2 vs 5
+[2, 5, 6, 8, 4]
+
+i = 4  ||  arr[3] vs arr[4]  ||  4 vs 8
+[2, 5, 6, 4, 8]
+i = 4  ||  arr[2] vs arr[3]  ||  4 vs 6
+[2, 5, 4, 6, 8]
+i = 4  ||  arr[1] vs arr[2]  ||  4 vs 5
+[2, 4, 5, 6, 8]
+i = 4  ||  arr[0] vs arr[1]  ||  2 vs 4
+[2, 4, 5, 6, 8]
+
+[2, 4, 5, 6, 8]
+
+Process finished with exit code 0
+
+
+
+     */
     private static int[] insertionSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             for (int j = i; j >= 1; j--) {
                 if (arr[j] < arr[j - 1]) {
                     swap(arr, j, j - 1);
                 }
+                System.out.println("i = " + i + "  ||  arr[" + (j - 1) + "] vs arr[" + (j) + "]" + "  ||  " + arr[j - 1] + " vs " + arr[(j)]);
+                System.out.println(Arrays.toString(arr));
             }
+            System.out.println();
         }
         return arr;
     }
 
-    private static int[] swap(int[] arr,int i,int j) {
+    private static int[] swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
