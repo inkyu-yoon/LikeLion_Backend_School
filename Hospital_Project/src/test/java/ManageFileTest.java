b450a4b94032cd39f0d8a0b324903da1114e14dc
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-14 11:32:34 +0900

    UPDATE : subdivision Test 추가

--- Hospital_Project/src/test/java/ManageFileTest.java
+++ Hospital_Project/src/test/java/ManageFileTest.java
@@ -14,7 +14,7 @@ class ManageFileTest {
     void SQL문테스트() {
         String oneLine = "\"A1106786\",\"서울특별시 송파구 동남로 208 (가락동)\",\"C\",\"의원\",\"G099\",\"응급의료기관 이외\",\"2\",\"토요일 첫째주 셋째주 휴진\",\"2005년 부터 진료 재활의학과전문의  전문물리치료 통증클리닉 척추교정치료 체외충격파 휜다리교정  목허리어깨무릎발 통증   디스크 퇴행성관절염\",\"극동아파트상가204호\",\"연세정소아과의원\",\"02-448-6436\",\"02-2227-7777\",\"1900\",\"1900\",\"1900\",\"1900\",\"1900\",\"1500\",\"1400\",\"1600\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"1000\",\"0900\",\"057\",\"83\",\"127.13147131787721\",\"37.49579508152157\",\"2022-09-07 14:55:30.0\"";
         HospitalParser hospitalParser = new HospitalParser();
-        Hospital hospital =hospitalParser.parse(oneLine);
+        Hospital hospital = hospitalParser.parse(oneLine);
         List<Hospital> list = new ArrayList<>();
         list.add(hospital);
         String sqlContents;
@@ -23,10 +23,22 @@ class ManageFileTest {
 
         String expected = "INSERT INTO `Hospital-DB`.`Seoul_Hospital`\n" +
                 "(`id`,`address`,`district`,`category`,`emergency_room`,`name`,`subdivision`)\n" +
-                "VALUES\n"+
+                "VALUES\n" +
                 "('A1106786','서울특별시 송파구 동남로 208 (가락동)','서울특별시 송파구','C','2','연세정소아과의원','소아 , 척추 , 교정 , 관절 , ');";
 
-        Assertions.assertEquals(sqlContents,expected);
+        Assertions.assertEquals(sqlContents, expected);
+
+
+    }
+
+    @Test
+    @DisplayName("subdivision 테스트")
+    void subdivision테스트(){
+        String oneLine = "\"A1106786\",\"서울특별시 송파구 동남로 208 (가락동)\",\"C\",\"의원\",\"G099\",\"응급의료기관 이외\",\"2\",\"토요일 첫째주 셋째주 휴진\",\"2005년 부터 진료 재활의학과전문의  전문물리치료 통증클리닉 척추교정치료 체외충격파 휜다리교정  목허리어깨무릎발 통증   디스크 퇴행성관절염\",\"극동아파트상가204호\",\"연세정소아과의원\",\"02-448-6436\",\"02-2227-7777\",\"1900\",\"1900\",\"1900\",\"1900\",\"1900\",\"1500\",\"1400\",\"1600\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"1000\",\"0900\",\"057\",\"83\",\"127.13147131787721\",\"37.49579508152157\",\"2022-09-07 14:55:30.0\"";
+        HospitalParser hospitalParser = new HospitalParser();
+        Hospital hospital = hospitalParser.parse(oneLine);
+
+        Assertions.assertEquals(hospital.getSubdivision(),"소아 , 척추 , 교정 , 관절 , ");
 
 
     }
