5f1bed56fe83702c9c54842b81cd58bd8339a88c
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-10-17 09:08:02 +0900

    ADD : 별찍기

---
+++ LikeLionClass/October/Lecture/Date221017/Algorithm/MakeStar.java
@@ -0,0 +1,10 @@
+package October.Lecture.Date221017.Algorithm;
+
+public class MakeStar {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
