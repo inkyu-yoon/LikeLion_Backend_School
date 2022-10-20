901cb4673b9a74528a1b14948bf8e16852f54c85
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-10-20 09:28:17 +0900

    UPDATE : Test code 작성

--- Stack_Project/src/main/java/stack/StackImpl.java
+++ Stack_Project/src/main/java/stack/StackImpl.java
@@ -9,16 +9,19 @@ public class StackImpl {
     }
     //스택 사이즈 직접 지정
     public StackImpl(int size) {
+
         this.stack = new int[size];
     }
 
     //스택에 값 집어넣기
     public void push(int value) {
+
         this.stack[++this.pointer] = value;
     }
 
     //스택 배열 반환하기
     public int[] getStack() {
+
         return stack;
     }
 
@@ -26,8 +29,18 @@ public class StackImpl {
     public int getArrById(int index){
         return stack[index];
     }
+    public int[] getArr(int index){
+        return stack;
+    }
 
     public int pop() {
         return stack[pointer--];
+    }
+    public boolean isEmpty(){
+        if (pointer == -1) {
+            return true;
+        }
+
+        return false;
     }
 }
