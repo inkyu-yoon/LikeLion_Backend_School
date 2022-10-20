e83bdddd5bf0083967998cf5792a6a0f01afcf0d
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-10-20 09:21:44 +0900

    UPDATE : Test code 작성

--- Stack_Project/src/test/java/stack/StackImplTest.java
+++ Stack_Project/src/test/java/stack/StackImplTest.java
@@ -1,12 +1,22 @@
 package stack;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackImplTest {
+
+    @BeforeEach
+    void setup(){
+        System.out.println("before each 실행");
+        //각 테스트 하기전에 실행되는 구현부는 @BeforeEach에 넣기
+    }
+
+
 
     @Test
     @DisplayName("push 테스트")
@@ -14,23 +24,24 @@ class StackImplTest {
         StackImpl stack = new StackImpl();
         stack.push(100);
         stack.push(200);
-        Assertions.assertEquals(stack.getArrById(0), 100);
-        Assertions.assertEquals(stack.getArrById(1), 200);
+
+        assertThat(100).isEqualTo(stack.getArrById(0));
+        assertThat(200).isEqualTo(stack.getArrById(1));
+        //junit 말고 assertj 라이브러리 사용
     }
 
     @Test
-    @DisplayName("push & pop 테스트")
-    void 푸쉬랑팝테스트() {
+    @DisplayName("pop 테스트")
+    void 팝테스트() {
         StackImpl stack = new StackImpl();
         int firstInput = 100;
         int secondInput = 200;
         stack.push(firstInput);
         stack.push(secondInput);
 
+        assertThat(secondInput).isEqualTo(stack.pop());
+        assertThat(firstInput).isEqualTo(stack.pop());
 
-        Assertions.assertEquals(secondInput, stack.pop());
-        Assertions.assertEquals(firstInput, stack.pop());
-
-
+        //스택은 마지막에 입력된게 먼저 나온다.
     }
 }
