99afc3786d65f8c2d2c05c02a983773a69e141ce
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-10-21 10:35:09 +0900

    UPDATE : 테스트 코드 추가

---
+++ Stack_Project/src/test/java/SolutionTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @Test
+    @DisplayName("스택 안쓰고")
+    void solution() {
+        Assertions.assertFalse(solution.solution("{}()[](({))"));
+        Assertions.assertTrue(solution.solution("{}()[](({}))"));
+    }
+
+    @Test
+    @DisplayName("replace로")
+    void solution2() {
+        Assertions.assertFalse(solution.solution2("{}()[](({))"));
+        Assertions.assertTrue(solution.solution2("{}()[](({}))"));
+    }
+
+    @Test
+    @DisplayName("스택으로")
+    void solution3() {
+        Assertions.assertFalse(solution.solution3("{}()[](({))"));
+        Assertions.assertTrue(solution.solution3("{}()[](({}))"));
+    }
+}
