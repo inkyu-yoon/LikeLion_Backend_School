5588cf8f959ad9cc5c4a625ae233e2a93e3d8131
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-10-24 10:27:55 +0900

    ADD : 가장 큰 수 + 우선순위 큐

--- LikeLionClass/October/Lecture/Date221024/Algorithm/Solution.java
+++ LikeLionClass/October/Lecture/Date221024/Algorithm/Solution.java
@@ -1,6 +1,7 @@
 package October.Lecture.Date221024.Algorithm;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
@@ -19,6 +20,23 @@ class Solution {
         return answer;
     }
 
+    public int[] solution2(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0;i< commands.length;i++){
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for(int j=commands[i][0]-1;j<=commands[i][1]-1;j++) {
+                pq.offer(array[j]);
+            }
+            int result =-1;
+            for(int j=0;j< commands[i][2];j++){
+                result = pq.poll();
+            }
+            answer[i]=result;
+        }
+
+
+        return answer;
+    }
     public String solution(int[] numbers) {
 
         StringBuilder sb = new StringBuilder();
