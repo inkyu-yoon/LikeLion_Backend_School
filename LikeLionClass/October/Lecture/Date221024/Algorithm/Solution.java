07057ca738bfcbad3334c233db6cc5d5c2465b12
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-10-24 10:25:14 +0900

    ADD : 가장 큰 수

--- LikeLionClass/October/Lecture/Date221024/Algorithm/Solution.java
+++ LikeLionClass/October/Lecture/Date221024/Algorithm/Solution.java
@@ -6,16 +6,47 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         int[] sliced;
-        int index =0;
+        int index = 0;
         for (int[] command : commands) {
             int i = command[0];
             int j = command[1];
             int k = command[2];
-            Arrays.sort(sliced=Arrays.copyOfRange(array,i-1,j));
-            answer[index++]=sliced[k-1];
+            Arrays.sort(sliced = Arrays.copyOfRange(array, i - 1, j));
+            answer[index++] = sliced[k - 1];
         }
 
 
         return answer;
+    }
+
+    public String solution(int[] numbers) {
+
+        StringBuilder sb = new StringBuilder();
+        String[] arr = new String[numbers.length];
+        int index = 0;
+        for (int number : numbers) {
+            arr[index++] = String.valueOf(number);
+        }
+        Arrays.sort(arr, (a, b) -> {
+            if (a.charAt(0) == b.charAt(0)) {
+                return (a+b).compareTo(b+a);
+            }
+            return a.compareTo(b);
+        });
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+
+        if(sb.charAt(0)=='0'){
+            return "0";
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {0,0,0,0};
+        solution.solution(arr);
+        System.out.println("solution.solution(arr) = " + solution.solution(arr));
     }
 }
