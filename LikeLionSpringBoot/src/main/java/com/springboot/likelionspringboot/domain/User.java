227d1c439344b7c8e9d8d5ad48c788b3c0e54306
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-10-28 15:14:54 +0900

    UPDATE : Lombok 으로 getter seteer 적용

--- LikeLionSpringBoot/src/main/java/com/springboot/likelionspringboot/domain/User.java
+++ LikeLionSpringBoot/src/main/java/com/springboot/likelionspringboot/domain/User.java
@@ -1,5 +1,11 @@
 package com.springboot.likelionspringboot.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
 public class User {
     private String id;
     private String name;
@@ -14,33 +20,7 @@ public class User {
                 '}';
     }
 
-    public User(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
