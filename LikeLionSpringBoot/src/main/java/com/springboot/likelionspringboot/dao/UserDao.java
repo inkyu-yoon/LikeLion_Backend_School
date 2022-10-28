a8505d3983c94f0634dd0919d7225ab88428d082
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-10-28 13:39:13 +0900

    ADD : User,UserDao 만듬 UserController 구성 중

---
+++ LikeLionSpringBoot/src/main/java/com/springboot/likelionspringboot/dao/UserDao.java
@@ -0,0 +1,31 @@
+package com.springboot.likelionspringboot.dao;
+
+import com.springboot.likelionspringboot.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class UserDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+
+    }
+
+    public void add(User user) {
+        jdbcTemplate.update("INSERT INTO USERS(id,name,password) VALUES (?,?,?)", user.getId(), user.getName(), user.getPassword());
+    }
+
+    public void delete(String id) {
+        jdbcTemplate.update("DELETE * FROM USERS WHERE id = ?", id);
+    }
+    public void deleteAll() {
+        jdbcTemplate.update("DELETE * FROM USERS");
+    }
+
+
+}
