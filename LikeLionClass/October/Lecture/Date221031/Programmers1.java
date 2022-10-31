f93874fd29eb0038270bd94317e7d6cba06a9482
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-10-31 09:57:09 +0900

    UPDATE : 프로그래머스 [모의고사] 리팩토링

--- LikeLionClass/October/Lecture/Date221031/Programmers1.java
+++ LikeLionClass/October/Lecture/Date221031/Programmers1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Programmers1 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] answers = {1, 2,3,4,5};
+        int[] answers = {2,3,4,5,1};
         int[] solution1 = solution.solution(answers);
         System.out.println(Arrays.toString(solution1));
 
@@ -16,27 +16,25 @@ class Solution {
     //수포자 1 : 1 2 3 4 5
     //수포자 2 : 2 1 2 3 2 4 2 5
     //수포자 3 : 3 3 1 1 2 2 4 4 5 5
-    public int[] solution(int[] answers) {
         List<Integer> answer = new ArrayList<>();
+        int number = 1;
+    public int[] solution(int[] answers) {
         int[] Man1 = {1, 2, 3, 4, 5};
         int[] Man2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] Man3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5,};
-
 
         int Man1Score = countScore(answers, Man1);
         int Man2Score = countScore(answers, Man2);
         int Man3Score = countScore(answers, Man3);
 
+        int [] Scores = {Man1Score,Man2Score,Man3Score};
         int maxScore = Math.max(Man1Score, Math.max(Man2Score, Man3Score));
-        if (Man1Score >= maxScore) {
-            answer.add(1);
+
+        for (int score : Scores) {
+        addRank(maxScore, score);
         }
-        if (Man2Score >= maxScore) {
-            answer.add(2);
-        }
-        if (Man3Score >= maxScore) {
-            answer.add(3);
-        }
+
+
         int[] result = new int[answer.size()];
         for (int i = 0; i < answer.size(); i++) {
             result[i] = answer.get(i);
@@ -44,6 +42,12 @@ class Solution {
         return result;
     }
 
+    void addRank(int MaxScore, int ManScore) {
+        if (ManScore >= MaxScore) {
+            answer.add(number);
+        }
+        number++;
+    }
     int countScore(int[] answers, int[] Man) {
         int count = 0;
         for (int i = 0; i < answers.length; i++) {
