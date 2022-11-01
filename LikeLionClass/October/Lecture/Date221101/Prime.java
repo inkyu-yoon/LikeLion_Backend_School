3c4bd3954a2eb62e7149bea139b980233a83fe92
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-01 09:32:20 +0900

    ADD : 소수 구하는 알고리즘 구현

---
+++ LikeLionClass/October/Lecture/Date221101/Prime.java
@@ -0,0 +1,33 @@
+package October.Lecture.Date221101;
+
+import java.util.Scanner;
+
+public class Prime {
+    //에라토스테네스의 체 구현
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        boolean[] primes = new boolean[N+2];
+        primes[0]=true;
+        primes[1]=true;
+
+        for (int i = 2; i*i <= N ; i++) {
+            for (int j = i*i; j <=N ; j+=i) {
+                primes[j]=true;
+            }
+        }
+
+        for (int i = 0; i <= N; i++) {
+            if(!primes[i]){
+                System.out.println(i);
+            }
+        }
+
+
+
+
+    }
+
+
+}
