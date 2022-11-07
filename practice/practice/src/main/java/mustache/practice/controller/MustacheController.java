a37738f34e053de7b048accba6a1899d761de991
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-07 11:46:29 +0900

    UPDATE : PathVariable 사용해서 응용

--- practice/practice/src/main/java/mustache/practice/controller/MustacheController.java
+++ practice/practice/src/main/java/mustache/practice/controller/MustacheController.java
@@ -4,13 +4,15 @@ import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MustacheController {
 
-    @GetMapping(value = "/hi")
-    public String mustacheCon(Model model) {
+    @GetMapping(value = "/hi/{id}")
+    public String mustacheCon(@PathVariable(name = "id") String id, Model model) {
         model.addAttribute("username","inkyu");
+        model.addAttribute("id",id);
         return "greetings";
     }
 }
