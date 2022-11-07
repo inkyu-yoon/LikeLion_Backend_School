ea70d7660256a401e6d9c9cfe486c9a68a703766
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-07 09:11:50 +0900

    UPDATE : 나누어 떨어지는지 체크

--- LikeLionClass/October/Lecture/Date221107/Problem1.java
+++ LikeLionClass/October/Lecture/Date221107/Problem1.java
@@ -2,13 +2,14 @@ package October.Lecture.Date221107;
 
 public class Problem1 {
     public static void main(String[] args) {
-        int num1 = 12345;
-
+        int num1 = 687;
+        int num2 = num1;
         int result =0;
         while (num1 > 0) {
             result += num1 % 10;
             num1/=10;
         }
-        System.out.println(result);
+        System.out.println("자릿수합: "+result);
+        System.out.println(num2%result==0?"하샤드수 맞음":"하샤드수 아님");
     }
 }
