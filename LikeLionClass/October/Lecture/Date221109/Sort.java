f4d4e310ee359fef19e976f27b1ed2ab895a6b7b
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-09 10:29:35 +0900

    UPDATE : 템플릿 콜백 적용

--- LikeLionClass/October/Lecture/Date221109/Sort.java
+++ LikeLionClass/October/Lecture/Date221109/Sort.java
@@ -5,14 +5,35 @@ import java.util.Arrays;
 public class Sort {
     public static void main(String[] args) {
         //오름차순
-        Selection selectionSort = new SelectionAsc();
-        int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
-        arr = selectionSort.sort(arr);
-        System.out.println(Arrays.toString(arr));
 
-        //내림차순
-        selectionSort = new SelectionDesc();
-        arr = selectionSort.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+
+        selectionSort(arr, (min, num) -> min > num);
+        selectionSort(arr, (max, num) -> max < num);
+
+
     }
+    static int[] selectionSort(int[] arr, StatementStrategy stmt) {
+        for (int i = 0; i < arr.length; i++) {
+            int maxOrMin = arr[i];
+            int maxOrMinIndex = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (stmt.apply(maxOrMin , arr[j])) {
+                    maxOrMin = arr[j];
+                    maxOrMinIndex = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = maxOrMin;
+            arr[maxOrMinIndex] = tmp;
+
+
+            System.out.println("i : " + i);
+            System.out.println(Arrays.toString(arr));
+        }
+        return arr;
+    }
+}
+interface StatementStrategy{
+    boolean apply(int maxOrMin, int num);
 }
