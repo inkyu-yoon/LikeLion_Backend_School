24b2ec6c5a817e7c0f0874a46a9bf0d5b91395ac
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-10 09:14:02 +0900

    ADD: 같은 숫자는 싫어

---
+++ LikeLionClass/October/Lecture/Date221110/Solution.java
@@ -0,0 +1,29 @@
+package October.Lecture.Date221110;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
+        System.out.println(Arrays.toString(s.solution(arr)));
+
+    }
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for (int i = 0; i < arr.length-1; i++) {
+            int tmp = arr[i];
+            if (arr[i] != arr[i + 1]) {
+                tmp = arr[i + 1];
+                list.add(tmp);
+            }
+        }
+        answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
