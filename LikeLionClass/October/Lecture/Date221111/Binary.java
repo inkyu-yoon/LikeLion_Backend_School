16ca5b80858bea270577804b143d844e59b0c0ad
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-11 09:49:29 +0900

    UPDATE : 10진수 -> 2진수 변경 구현

--- LikeLionClass/October/Lecture/Date221111/Binary.java
+++ LikeLionClass/October/Lecture/Date221111/Binary.java
@@ -1,14 +1,18 @@
 package October.Lecture.Date221111;
 
 public class Binary {
+    public static void main(String[] args) {
+        System.out.println(getBinaryString(25));
+    }
 
-    String getBinaryString(int decNum) {
-        String answer = "";
+    static String getBinaryString(int decNum) {
+
+        StringBuilder sb = new StringBuilder();
         while (decNum > 0) {
-            answer += decNum % 2;
+            sb.append(decNum % 2);
             decNum /= 2;
         }
-        return answer;
+        return sb.reverse().toString();
 
     }
 }
