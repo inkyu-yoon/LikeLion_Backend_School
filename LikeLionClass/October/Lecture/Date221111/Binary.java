228a866c1d30c0e40da521811e1332a2ff876b2f
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-11 09:52:26 +0900

    UPDATE : 개수에 맞게 구현

--- LikeLionClass/October/Lecture/Date221111/Binary.java
+++ LikeLionClass/October/Lecture/Date221111/Binary.java
@@ -2,15 +2,18 @@ package October.Lecture.Date221111;
 
 public class Binary {
     public static void main(String[] args) {
-        System.out.println(getBinaryString(25));
+        System.out.println(getBinaryString(9,6));
     }
 
-    static String getBinaryString(int decNum) {
+    static String getBinaryString(int decNum,int size) {
 
         StringBuilder sb = new StringBuilder();
         while (decNum > 0) {
             sb.append(decNum % 2);
             decNum /= 2;
+        }
+        while (sb.length() < size) {
+            sb.append(0);
         }
         return sb.reverse().toString();
 
