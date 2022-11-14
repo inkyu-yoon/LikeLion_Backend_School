9a3c2d0c4c1f666e676413bf0e418c7e691ce83a
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-14 13:19:34 +0900

    ADD : 경기도 수원시 병원 검색 sql 구문

---
+++ practice/practice/src/main/java/mustache/practice/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package mustache.practice.repository;
+
+import mustache.practice.domain.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+}
+
+
