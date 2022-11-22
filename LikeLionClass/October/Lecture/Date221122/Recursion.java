b3f3509413202f43f25f3e38004aac4c5eb3bb81
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-22 10:06:23 +0900

    ADD : 재귀로 각 자리수의 합 구하기

--- LikeLionClass/October/Lecture/Date221122/Recursion.java
+++ LikeLionClass/October/Lecture/Date221122/Recursion.java
@@ -6,12 +6,22 @@ public class Recursion {
 
     public static void main(String[] args) {
         print1To100(1);
-        System.out.println("arraySum(arr.length-1) = " + arraySum(arr.length-1));
+        System.out.println("arraySum(arr.length-1) = " + arraySum(arr.length - 1));
         printStar(3);
         System.out.println();
         System.out.println("getSum(10) = " + getSum(10));
+        System.out.println("getSum2(1234) = " + getSum2(1234));
     }
 
+    // 재귀로 각 자리 수의 합 구하기
+    static long getSum2(long n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n%10 + getSum2(n / 10);
+    }
+
+    //1 부터 n 까지 재귀로 합 구하기
     static int getSum(int n) {
         if (n == 1) {
             return 1;
@@ -26,7 +36,7 @@ public class Recursion {
             return;
         }
         System.out.print("*");
-        printStar(n-1);
+        printStar(n - 1);
 
     }
 
