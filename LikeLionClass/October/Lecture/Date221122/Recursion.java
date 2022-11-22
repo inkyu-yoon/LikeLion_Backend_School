ed44e0131b7f8b26cb279aee5a48dea176880f0b
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-22 09:15:19 +0900

    ADD : print1To100 자기 자신 호출하기

---
+++ LikeLionClass/October/Lecture/Date221122/Recursion.java
@@ -0,0 +1,13 @@
+package October.Lecture.Date221122;
+
+public class Recursion {
+    static void print1To100(int n) {
+        System.out.println(n);
+        print1To100(n);
+    }
+
+    public static void main(String[] args) {
+        print1To100(1);
+
+    }
+}
