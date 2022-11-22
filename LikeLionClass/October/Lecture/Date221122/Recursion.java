87b450e94e4ad948173bceb19ae932bbef491f43
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-22 09:56:31 +0900

    ADD : 배열 합 재귀

--- LikeLionClass/October/Lecture/Date221122/Recursion.java
+++ LikeLionClass/October/Lecture/Date221122/Recursion.java
@@ -1,16 +1,30 @@
 package October.Lecture.Date221122;
 
 public class Recursion {
+
+    static int[] arr = {7, 3, 2, 9};
+
+    public static void main(String[] args) {
+        print1To100(1);
+        System.out.println("arraySum(arr.length-1) = " + arraySum(arr.length-1));
+    }
+
+
+    // 배열 합 재귀
+    static int arraySum(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        return arr[n] + arraySum(n - 1);
+    }
+
+    //출력 재귀
     static void print1To100(int n) {
         if (n > 100) {
             return;
         }
         System.out.println(n);
-        print1To100(n+1);
+        print1To100(n + 1);
     }
 
-    public static void main(String[] args) {
-        print1To100(1);
-
-    }
 }
